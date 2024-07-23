c187bc64effee63739e61b95cce7e70f174c1349
tewarishash12 <122259612+tewarishash12@users.noreply.github.com>
2024-07-23 14:19:27 +0000

    reduce function

---
+++ reduce.js
@@ -0,0 +1,26 @@
+// const numbers = [12,7,8,1,2,15,3,4,5,11];
+
+// const total = numbers.reduce((accumulator, value)=>{
+//     if(accumulator>value)
+//         return accumulator;
+//     return value;
+// });
+
+// console.log(total)
+
+const items =[
+    {
+        name: 'laptop',
+        price: 60000,
+        count: 5
+    },{
+        name: 'desktop',
+        price: 70000,
+        count: 10
+    }, {
+        name: 'phone',
+        price: 20000,
+        count: 15
+    }
+];
+
